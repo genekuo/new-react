2a6decf0e3d3211b9044cf8d7bc829bd5495b125
KuoYu-chiung <genekuo@cybersoft4u.com>
2019-11-01 10:38:19 +0800

    initial repo

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import * as React from 'react';
+import {Popout} from 'react-popout-component';
+
+export default class App extends React.Component<any, any> {
+    constructor(props: any) {
+        super(props);
+        this.state = {showPopout: false};
+    }
+
+    onClick = () => {
+        this.setState({showPopout: true});
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Now you too have the power to POP OUT</h1>
+                <button onClick={this.onClick}>POP IT OUT!</button>
+                {this.state.showPopout && (
+                    <Popout>
+                        <div>You can put anything here!</div>
+                    </Popout>
+                )}
+            </div>
+        );
+    }
+}
